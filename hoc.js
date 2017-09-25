28c5de4c2a53a3e1bf6b9c640d6a800bf25537e4
jiajianrong <jiajianrong@58.com>
2017-09-25 20:51:03 +0800

    hoc

---
+++ hoc.js
@@ -0,0 +1,87 @@
+// demo1: hardcode middleware function
+var f = decorate(getPrice, [getDiscount, getDiscount, logger])
+console.log(f())
+
+
+// demo2: customize middleware function parameters
+var ff = decorate(getPrice, [discountFactory(0.9), discountFactory(0.8), logger])
+console.log(ff())
+
+
+
+/**
+ * 包裹打印功能
+ */
+function logger(f) {
+
+	return function(){
+		console.log('logger start')
+		var r = f()
+		console.log('logger end')
+		return r
+	}
+	
+}
+
+
+
+/**
+ * 包裹固定折扣
+ */
+function getDiscount(f) {
+	
+	return function(){
+		console.log('getDiscount')
+		return f() * 0.9
+	}
+
+}
+
+
+
+/**
+ * 自定义折扣 工厂方法
+ */
+function discountFactory(discount) {
+
+	function getDiscount(f) {
+		
+		return function(){
+			console.log('getDiscount')
+			return f() * discount
+		}
+
+	}
+	
+	return getDiscount
+
+}
+
+
+
+/**
+ * 目标方法 获取价格
+ */
+function getPrice() {
+	console.log('getPrice')
+	return 100;
+}
+
+
+
+
+/**
+ * middleware implement
+ */
+function decorate(target, wrappers) {
+	return wrappers.reverse().concat(getPrice).reverse().reduce(function(p, c){
+		return c(p)
+	})
+}
+
+
+
+
+
+
+
