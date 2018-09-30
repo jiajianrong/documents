1ad87a1656ed7f6f6f5f01e0327b88f4777f1250
jiajianrong <jiajianrong@58ganji.com>
2018-09-30 19:49:20 +0800

    400

--- node/net-mock-http.js
+++ node/net-mock-http.js
@@ -1,13 +1,21 @@
 const net = require('net');
 
+
 var content = 
 `HTTP/1.1 200 OK
 Content-Type: application/json;charset=UTF-8
 
 {"code":0}`;
 
+
+var badContent = 
+'HTTP/1.1 400 Bad Request\r\n' +
+'Content-Length: 0\r\n' +
+'\r\n';
+
+
 net.createServer((socket) => {
-    socket.end(content);
+    socket.end(badContent);
 }).on('error', (err) => {
     console.log('err occurs', err);
     throw err;
