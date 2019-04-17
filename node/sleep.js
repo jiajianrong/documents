259e0b3af8c2567ac246f68912fca7467ec57f61
jiajianrong <>
2019-04-17 10:30:49 +0800

    sleep demo

---
+++ node/sleep.js
@@ -0,0 +1,27 @@
+﻿function sleep(n) {
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, n);
+    }).catch(() => console.log('Oops errors!'));
+    
+}
+
+
+function sleepRandom() {
+    
+    return new Promise((resolve, reject) => {
+        setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+    }).catch(() => console.log('Oops errors!'));
+    
+}
+
+
+
+async function main() {
+    
+    console.log('start')
+    await sleepRandom()
+    console.log('end')
+}
+
+main()
